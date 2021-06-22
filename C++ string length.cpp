1fafa4dc7325d181c65dc630a0edb3aca231f8da
G.M. Arafat Rahman <62995847+arafat04@users.noreply.github.com>
2021-06-23 00:51:17 +0600

    use of length() method of cpp string class

---
+++ C++ string length.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // we wil use built in string class method length() to get length of a string
+    //some text
+    //012345678
+
+    string s = "some text";
+    cout<<s.length();
+    return 0;
+}
